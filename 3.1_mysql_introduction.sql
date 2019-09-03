46a2ea85fc4d43aac31323e9d9c2dacf94c9738e
soslin <seanoslin@gmail.com>
2019-09-03 14:20:07 -0500

    first sequal exercises

---
+++ 3.1_mysql_introduction.sql
@@ -0,0 +1,8 @@
+show databases;
+
+-- mysql.user means use the mysql database and read from the user table--
+select * from mysql.user;
+
+select user, host from mysql.user;
+select * from mysql.help_topic;
+
